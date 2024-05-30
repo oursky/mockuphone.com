92302cf4828fdd22796e926e1e1dc1b1e06c4b8f
pkong-ds <pkong.work@gmail.com>
2024-05-30 15:18:38 +0800

    Use algolia's search input instead of custom search input

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -9,12 +9,14 @@ ready(main);
 
 class RootViewModel {
   searchText = "";
+  _brandThumbnailList;
 
-  constructor() {
+  constructor(brandThumbnailList) {
     mobx.makeObservable(this, {
       searchText: mobx.observable,
       shouldShowSearchClear: mobx.computed,
     });
+    this._brandThumbnailList = brandThumbnailList;
   }
 
   get shouldShowSearchClear() {
@@ -23,45 +25,49 @@ class RootViewModel {
 }
 
 function handleSearchInput(viewModel) {
-  const searchInput = document.querySelector(".search-device__input");
+  const searchInput = document.querySelector(".aa-Input");
   searchInput.addEventListener("input", (e) => {
     viewModel.searchText = e.target.value;
   });
-
-  const searchContainer = document.querySelector(".search-device__container");
-  searchContainer.addEventListener("click", () => {
-    searchInput.focus();
-  });
 }
 
-function handleSearchClearBtn(viewModel) {
-  const searchInput = document.querySelector(".search-device__input");
-  const searchClearBtn = document.querySelector(".search-device__clear-btn");
-  searchClearBtn.addEventListener("click", () => {
-    viewModel.searchText = "";
+function initializeAutocomplete(viewModel) {
+  const { autocomplete } = window["@algolia/autocomplete-js"];
+  autocomplete({
+    container: "#homepage-autocomplete",
+    placeholder: "Search Device",
+    // FIXME: provide real sources
+    getSources() {
+      return [
+        {
+          sourceId: "products",
+          getItems() {
+            return [{ name: "a" }, { name: "b" }];
+          },
+          templates: {
+            item({ item, components, html }) {
+              return html`<a
+                class="aa-ItemWrapper"
+                href="http://localhost:3000/model/iphone-14/color/blue"
+              >
+                ${item.name}
+              </a>`;
+            },
+          },
+        },
+      ];
+    },
   });
 
-  mobx.reaction(
-    () => viewModel.searchText,
-    () => {
-      searchInput.value = viewModel.searchText;
-      if (viewModel.shouldShowSearchClear) {
-        searchClearBtn.classList.remove("d-none");
-      } else {
-        searchClearBtn.classList.add("d-none");
-      }
-    },
-  );
+  handleSearchInput(viewModel);
 
-  tippy("[data-tippy-content]", {
+  tippy(".aa-ClearButton", {
+    content: "Clear",
     placement: "bottom",
     theme: "light-border",
   });
 }
-
 function main() {
-  const viewModel = new RootViewModel();
-
-  handleSearchInput(viewModel);
-  handleSearchClearBtn(viewModel);
+  const viewModel = new RootViewModel(window.brandThumbnailList);
+  initializeAutocomplete(viewModel);
 }
