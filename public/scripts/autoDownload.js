cbea25ddbd18a4a00829d2f8479d82d46e51cebe
yayunhuang <a29853935@gmail.com>
2024-08-14 16:10:48 +0800

    Auto download mockup when navigating to download page

---
+++ public/scripts/autoDownload.js
@@ -0,0 +1,12 @@
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+ready(main);
+
+function main() {
+  generateZIP(window.targetDeviceId);
+}
